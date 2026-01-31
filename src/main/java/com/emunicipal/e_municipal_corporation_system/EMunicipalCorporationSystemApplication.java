b8ee9aa6cd1c3c1e5dfd4abee91f3a6f60e50c31
Gaurav <gauravpbulbule@gmail.com>
2026-01-31 13:48:15 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/emunicipal/e_municipal_corporation_system/EMunicipalCorporationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.emunicipal.e_municipal_corporation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMunicipalCorporationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMunicipalCorporationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emunicipal/e_municipal_corporation_system/EMunicipalCorporationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emunicipal.e_municipal_corporation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMunicipalCorporationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
